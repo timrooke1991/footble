3bd817b0a22603167a6cb8f78520c19d2bafdda0
timrooke1991 <timrooke1991@gmail.com>
2022-07-20 17:50:31 +0100

    Basic wordle game refactored

--- game.js
+++ game.js
@@ -2,13 +2,9 @@ import {LitElement, html, css} from 'lit';
 import {range} from 'lit/directives/range.js';
 import {map} from 'lit/directives/map.js';
 
-const keyboard = [
-  ['q','w','e','r','t','y','u','i','o','p'],
-  ['a','s','d','f','g','h','j','k','l'],
-  ['z','x','c','v','b','n','m']
-];
+import './src/keyboard.js';
 
-const words = ['trunk', 'space', 'races', 'trend', 'coins', 'range', 'trees', 'eeeee', 'hello', 'eelle', 'eeele'];
+import { wordlist } from './src/words.js';
 
 export class Game extends LitElement {
   static get styles() {
@@ -90,43 +86,6 @@ export class Game extends LitElement {
         border: 2px solid #b59f3b;
         background-color: #b59f3b;
       }
-
-      #keyboard-container {
-        height: 190px;
-        padding-bottom: 10px;
-      }
-
-      .keyboard-row {
-        display: flex;
-        justify-content: center;
-        width: 100%;
-        margin: 0 auto 8px;
-        touch-action: manipulation;
-      }
-
-      .keyboard-row button {
-        font-family: inherit;
-        font-weight: bold;
-        border: 0;
-        padding: 0;
-        height: 58px;
-        cursor: pointer;
-        background-color: rgb(129, 131, 132);
-        color: rgb(215, 218, 220);
-        flex-grow: 1;
-        text-transform: uppercase;
-        margin-right: 6px;
-        border-radius: 4px;
-        user-select: none;
-      }
-
-      .keyboard-row button.wide-button {
-        flex-grow: 1.5;
-      }
-
-      .spacer-half {
-        flex-grow: 0.5;
-      }
     `;
   }
 
@@ -143,16 +102,22 @@ export class Game extends LitElement {
     this.count = 0;
     this.guesses = [];
     this.currentGuess = '';
-    this.answer = words[Math.floor(Math.random() * words.length)];
+    this.answer = wordlist[Math.floor(Math.random() * wordlist.length)];
+  }
+
+
+  isLetter(key) {
+    return /^[a-z]+$/i.test(key);
+  }
+
+  getKeyFromEvent(e) {
+    return e.type === 'keydown' ? String.fromCharCode(e.which) : e.target.dataset.key;
   }
 
   selectLetter(e) {
-    console.log(e.target.dataset);
-    console.log(e.type)
-    const key = e.type === 'keypress' ? String.fromCharCode(e.which) : e.target.dataset.key;
- 
-    console.log(e.which);
-    console.log(this.currentGuess);
+    const key = this.getKeyFromEvent(e);
+
+    console.log(e.which, e.type);
     if (e.which === 8) return this.handleDelete(e);
 
     if (e.which === 13 && this.currentGuess.length === 5) {
@@ -163,12 +128,17 @@ export class Game extends LitElement {
       alert('Your word is already long enough');
     }
 
-    this.currentGuess = `${this.currentGuess}${key}`;
+
+    if (this.isLetter(key)) {
+      this.currentGuess = `${this.currentGuess}${key}`;
+    }
+
     return;
   }
 
   handleSubmit(e) {
-    const key = e.type === 'keypress' ? String.fromCharCode(e.which) : e.target.dataset.key;
+    e.preventDefault();
+    const key = this.getKeyFromEvent(e);
 
     if (key === 'enter' || e.which === 13) {
       if (this.currentGuess.length !== 5) {
@@ -176,7 +146,7 @@ export class Game extends LitElement {
         return 
       }
 
-      if (words.includes(this.currentGuess) === false) {
+      if (wordlist.includes(this.currentGuess) === false) {
         alert('Word is invalid');
         this.currentGuess = '';
         return;
@@ -194,7 +164,7 @@ export class Game extends LitElement {
   }
 
   handleDelete(e) {
-    const key = e.type === 'keypress' ? String.fromCharCode(e.which) : e.target.dataset.key;
+    const key = this.getKeyFromEvent(e);
     if (key === 'del' || e.which === 8) {
       console.log('deleted');
       this.currentGuess = this.currentGuess.slice(0, -1); 
@@ -210,38 +180,27 @@ export class Game extends LitElement {
             <h1 class="title">WORDLE</h1>
           </header>
 
-      <div class="game-grid">
-        <div class="board">
-          ${map(range(6), (row) =>
-            map(
-              range(5),
-              (col) => 
-                row === this.count 
-                  ? html`<div class="game-tile">${getLabel(0, col, [this.currentGuess])}</div>`
-                  : html`<div class="game-tile ${getColor(row, col, this.answer, this.guesses)}">${getLabel(row, col, this.guesses)}</div>`
-              )
-          )}
+        <div class="game-grid">
+          <div class="board">
+            ${map(range(6), (row) =>
+              map(
+                range(5),
+                (col) => 
+                  row === this.count 
+                    ? html`<div class="game-tile">${getLabel(0, col, [this.currentGuess])}</div>`
+                    : html`<div class="game-tile ${getColor(row, col, this.answer, this.guesses)}">${getLabel(row, col, this.guesses)}</div>`
+                )
+            )}
+          </div>
         </div>
-      </div>
 
-      <div id="keyboard-container">
-          <div id="keyboard-container" >
-          <div class="keyboard-row">
-            ${map(keyboard[0], (letter) => html`<button   @keypress=${(e) => this.selectLetter(e)} @click=${this.selectLetter} data-key=${letter}>${letter}</button>`)}
-          </div>
-          <div class="keyboard-row">
-            <div class="spacer-half"></div>
-            ${map(keyboard[1], (letter) => html`<button @keypress=${(e) => this.selectLetter(e)} @click=${this.selectLetter} data-key=${letter}>${letter}</button>`)}
-            <div class="spacer-half"></div>
-          </div>
-          <div class="keyboard-row">
-            <button @click=${this.handleSubmit} data-key="enter" class="wide-button">Enter</button>
-            ${map(keyboard[2], (letter) => html`<button @keypress=${(e) => this.selectLetter(e)} @click=${this.selectLetter} data-key=${letter}>${letter}</button>`)}
-            <button @click=${this.handleDelete} data-key="del" class="wide-button">Del</button>
-          </div>
-        </div>
+      <game-keyboard 
+        .disabled="${this.currentGuess.length !== 5}" 
+        .clickHandler="${(e) => this.selectLetter(e)}"
+        .submitHandler="${(e) => this.handleSubmit(e)}"
+        .deleteHandler="${(e) => this.handleDelete(e)}"
+      ></game-keyboard>
       </div>
-        </div>
     </div>
     `;
   }
---
+++ src/keyboard.js
@@ -0,0 +1,85 @@
+import {LitElement, html, css} from 'lit';
+import {map} from 'lit/directives/map.js';
+
+const keyboard = [
+  ['q','w','e','r','t','y','u','i','o','p'],
+  ['a','s','d','f','g','h','j','k','l'],
+  ['z','x','c','v','b','n','m']
+];
+
+export class Keyboard extends LitElement {
+  static get styles() {
+    return css`
+      #keyboard-container {
+        height: 190px;
+        padding-bottom: 10px;
+      }
+
+      .keyboard-row {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        margin: 0 auto 8px;
+        touch-action: manipulation;
+      }
+
+      .keyboard-row button {
+        font-family: inherit;
+        font-weight: bold;
+        border: 0;
+        padding: 0;
+        height: 58px;
+        cursor: pointer;
+        background-color: rgb(129, 131, 132);
+        color: rgb(215, 218, 220);
+        flex-grow: 1;
+        text-transform: uppercase;
+        margin-right: 6px;
+        border-radius: 4px;
+        user-select: none;
+      }
+
+      .keyboard-row button.wide-button {
+        flex-grow: 1.5;
+      }
+
+      .spacer-half {
+        flex-grow: 0.5;
+      }
+    `;
+  }
+
+  constructor() {
+    super();
+    this.clickHandler = null;
+    this.submitHandler = null;
+    this.deleteHandler = null;
+    this.disabledSubmit = true;
+  }
+
+  render() {
+    return html`
+      <div id="keyboard-container">
+          <div id="keyboard-container" >
+          <div class="keyboard-row">
+            ${map(keyboard[0], (letter) => html`<button @keydown=${this.clickHandler} @click=${this.clickHandler} data-key=${letter}>${letter}</button>`)}
+          </div>
+          <div class="keyboard-row">
+            <div class="spacer-half"></div>
+            ${map(keyboard[1], (letter) => html`<button @keydown=${this.clickHandler} @click=${this.clickHandler} data-key=${letter}>${letter}</button>`)}
+            <div class="spacer-half"></div>
+          </div>
+          <div class="keyboard-row">
+            <button ?disabled="${this.disabledSubmit}" @click=${this.submitHandler} data-key="enter" class="wide-button">Enter</button>
+            ${map(keyboard[2], (letter) => html`<button @keydown=${this.clickHandler} @click=${this.clickHandler} data-key=${letter}>${letter}</button>`)}
+            <button @keydown=${this.deleteHandler} @click=${this.deleteHandler} data-key="del" class="wide-button">Del</button>
+          </div>
+        </div>
+      </div>
+        </div>
+    </div>
+    `;
+  }
+}
+
+window.customElements.define('game-keyboard', Keyboard);
